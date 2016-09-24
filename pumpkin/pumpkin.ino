856d2437a89d7a2863aa58a200fb6b28c36c499c
John M. Wargo <john@johnwargo.com>
2016-09-24 14:27:40 -0400

    added code folder

--- pumpkin/pumpkin.ino
+++ pumpkin/pumpkin.ino
@@ -1,11 +1,11 @@
 #include <Adafruit_NeoPixel.h>
 #ifdef __AVR__
-  #include <avr/power.h>
+#include <avr/power.h>
 #endif
 
 // Which pin on the Arduino is connected to the NeoPixels?
 // On a Trinket or Gemma we suggest changing this to 1
-#define PIN            6
+#define PIN            2
 
 // How many NeoPixels are attached to the Arduino?
 #define NUMPIXELS      16
@@ -31,10 +31,10 @@ void loop() {
 
   // For a set of NeoPixels the first NeoPixel is 0, second is 1, all the way up to the count of pixels minus one.
 
-  for(int i=0;i<NUMPIXELS;i++){
+  for (int i = 0; i < NUMPIXELS; i++) {
 
     // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
-    pixels.setPixelColor(i, pixels.Color(0,150,0)); // Moderately bright green color.
+    pixels.setPixelColor(i, pixels.Color(0, 150, 0)); // Moderately bright green color.
 
     pixels.show(); // This sends the updated pixel color to the hardware.
 
