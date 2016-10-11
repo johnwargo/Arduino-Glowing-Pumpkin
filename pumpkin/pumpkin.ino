7fc18059491e7e3df73586f8d0ae6366926c1155
John M. Wargo <john@johnwargo.com>
2016-10-11 17:46:04 -0400

    got the code working

--- pumpkin/pumpkin.ino
+++ pumpkin/pumpkin.ino
@@ -1,21 +1,38 @@
+/**************************************************
+   Glowing Pumpkin Project
+
+   By John M. Wargo
+   September 24, 2016
+ **************************************************/
+//===================
+// Adafruit Libs
+//===================
 #include <Adafruit_NeoPixel.h>
 #ifdef __AVR__
 #include <avr/power.h>
 #endif
 
-// Which pin on the Arduino is connected to the NeoPixels?
-// On a Trinket or Gemma we suggest changing this to 1
+// color definitions
+int cBlue[] = {0, 0, 255};
+int cGreen[] = {0, 255, 0};
+int cOrange[] = {255, 128, 1};
+int cPurple[] = {128, 0, 128};
+int cRed[] = {255, 0, 0};
+int cWhite[] = {255, 255, 255};
+int cYellow[] = {255, 215, 0};
+//colors is an array of all of the colors
+int numColors = 6;
+int colors[] = {cBlue, cGreen, cOrange, cPurple, cRed, cYellow};
+
+// specifies the Trinket pin the NeoPixel is connected to. For this project, it's pin 2.
 #define PIN            2
 
-// How many NeoPixels are attached to the Arduino?
+// Specifies the number of NeoPixels that are attached to the Trinket
+// Change this value if you're using a 12 LED ring or some other NeoPixel configuration
 #define NUMPIXELS      16
 
-// When we setup the NeoPixel library, we tell it how many pixels, and which pin to use to send signals.
-// Note that for older NeoPixel strips you might need to change the third parameter--see the strandtest
-// example for more information on possible values.
+// Let the NeoPixel library know how many LEDs we have and which pin it's connected to
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
-
-int delayval = 500; // delay for half a second
 
 void setup() {
   // This is for Trinket 5V 16MHz, you can remove these three lines if you are not using a Trinket
@@ -23,22 +40,60 @@ void setup() {
   if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
 #endif
   // End of trinket special code
-
   pixels.begin(); // This initializes the NeoPixel library.
+  //cut the brightness in half (1 to 256)
+  pixels.setBrightness(64);
+  pixels.show();
 }
 
 void loop() {
+  if ((int)random(10) > 8){
+    flicker();
+  } else {
+    //rotate the lights, picking a random color and a random delay value from 50-500
+    rotateLights(colors[(int)random(1, numColors)], (int)random(50, 500));
+  }  
+}
 
-  // For a set of NeoPixels the first NeoPixel is 0, second is 1, all the way up to the count of pixels minus one.
-
-  for (int i = 0; i < NUMPIXELS; i++) {
-
-    // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
-    pixels.setPixelColor(i, pixels.Color(0, 150, 0)); // Moderately bright green color.
-
-    pixels.show(); // This sends the updated pixel color to the hardware.
-
-    delay(delayval); // Delay for a period of time (in milliseconds).
-
+void flicker() {
+  // how many times are we going to flash?
+  int flashCount = (int)random(7);
+  //start flashing
+  for (int i = 0; i < flashCount; i++) {
+    // Set all pixels to white
+    setColor(pixels.Color(127, 127, 127));
+    delay((int)random(50, 150)); // Delay for a period of time (in milliseconds).
+    clearPixels();
+    delay((int)random(100, 500));
   }
 }
+
+void rotateLights(int c[], int delVal) {
+  for (int i = 0; i < NUMPIXELS; i++) {
+    pixels.setPixelColor(i, pixels.Color(c[0], c[1], c[2]));
+    pixels.show(); // This sends the updated pixel color to the hardware.
+    delay(delVal); // Delay for a period of time (in milliseconds).
+  }  
+}
+
+// Fill the dots one after the other with a color
+void setColor(uint32_t c) {
+  for (uint16_t i = 0; i < pixels.numPixels(); i++) {
+    pixels.setPixelColor(i, c);
+    pixels.show();
+  }
+}
+
+// Fill the dots one after the other with a color
+void colorWipe(uint32_t c, uint8_t wait) {
+  for (uint16_t i = 0; i < pixels.numPixels(); i++) {
+    pixels.setPixelColor(i, c);
+    pixels.show();
+    delay(wait);
+  }
+}
+
+void clearPixels() {
+  colorWipe(pixels.Color(0, 0, 0), 0);
+}
+
